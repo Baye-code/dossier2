437dc6416023e50d98079a1beb16d4874a9f4c1f
Baye LAM <bayesabarane@gmail.com>
2024-02-19 10:14:37 +0000

    connexion a l'equipement et affichage des vlans

--- infra_script.py
+++ infra_script.py
@@ -5,7 +5,12 @@ username = input("Veuillez entrer votre nom d'utilisateur: ")
 password = getpass.getpass("Veuillez entrer votre mot de passe")
 
 HOST = {
-
+	"ip": "192.168.10.10",
+	"device_type": "cisco",
+	"username": username,
+	"password": password
 }
 
 connect = ConnectHandler(**HOST)
+
+connect.send_command("sh vlan brief")
